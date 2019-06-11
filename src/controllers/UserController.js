bb711cade7740c0a4e94e9b5039b16e0351b5661
null <>
2019-06-11 14:24:20 -0300

    validações de chamadas e inclusão de métodos, put, delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,13 +2,29 @@ const User = require('../models/Users');
 
 class UserController {
     async create(req, res) {
-        const user = await User.create(req.body);
-
-        return res.json(user);
+        const user = await User.create(req.body, (err, success) => {
+            if (err) return res.status(400).send({
+                message: 'Não foi possível criar usuário, verifique as informações enviadas',
+                details: err
+            })
+            return res.send({
+                message: 'Usuário criado com sucesso!',
+                userRemoved: req.params.id
+            })
+        });
     }
 
     async findById(req, res) {
-        const user = await User.findById(req.params.id);
+        const user = await User.findById(req.params.id, (err, success) => {
+            if (err) return res.status(400).send({
+                message: 'Não foi possível localizar usuário, verifique o id',
+                details: err
+            })
+            return res.send({
+                message: 'Usuário!',
+                userRemoved: req.params.id
+            })
+        });
 
         return res.json(user);
     }
@@ -18,6 +34,34 @@ class UserController {
 
         return res.json(user);
     }
+
+    async del(req, res) {
+        const user = await User.findByIdAndDelete(req.params.id, (err, success) => {
+            if (err) return res.status(400).send({
+                message: 'Não foi possível remover registro, verifique o id solicitado!',
+                details: err
+            })
+            return res.send({
+                message: 'Usuário removido com sucesso!',
+                userRemoved: req.params.id
+            })
+        });
+    }
+
+    async update(req, res) {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        }, (err, success) => {
+            if (err) return res.status(400).send({
+                message: 'Não foi possível atualizar registro, verifique as informações enviadas!'
+            })
+            return res.send({
+                message: 'USuário atualizado com sucesso!'
+            })
+        });
+    }
+
+
 }
 
 module.exports = new UserController();
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,8 @@ const routes = express.Router();
 routes.post('/user', UserController.create);
 routes.get('/user/:id', UserController.findById);
 routes.get('/user', UserController.find);
+routes.delete('/user/:id', UserController.del);
+routes.put('/user/:id', UserController.update);
+
 
 module.exports = routes;
